6d76ad52e8bdf78bae06b336487010973c59eb96
Michael Hsueh <hsueh91@gmail.com>
2013-04-07 09:55:06 -0400

    removed some unused code

--- src/extractor/EventExtractor.java
+++ src/extractor/EventExtractor.java
@@ -27,47 +27,61 @@ public class EventExtractor {
 		if (timestring.equals("")) {
 			return timestring;
 		}
-		DateTime datetime = ISODateTimeFormat.dateTimeNoMillis().parseDateTime(timestring);
-		DateTimeFormatter fmt = DateTimeFormat.forPattern("EEEE, MMMM d, YYYY h:mm a");
+		DateTime datetime = ISODateTimeFormat.dateTimeNoMillis().parseDateTime(
+				timestring);
+		DateTimeFormatter fmt = DateTimeFormat
+				.forPattern("EEEE, MMMM d, YYYY h:mm a");
 		return fmt.print(datetime);
 	}
-	
+
 	/**
 	 * 
-	 * @param sortKey the key to sort entities by
-	 * @param sortDirection ordering of results
+	 * @param sortKey
+	 *            the key to sort entities by
+	 * @param sortDirection
+	 *            ordering of results
 	 * @return
 	 */
-	public static List<Entity> retrieve(String sortKey, String sortDirection, int limit) {
-		SortDirection dir = (sortDirection.equalsIgnoreCase("ascending"))? SortDirection.ASCENDING :SortDirection.DESCENDING;
+	public static List<Entity> retrieve(String sortKey, String sortDirection,
+			int limit) {
+		SortDirection dir = (sortDirection.equalsIgnoreCase("ascending")) ? SortDirection.ASCENDING
+				: SortDirection.DESCENDING;
 		Query query = new Query("event").addSort(sortKey, dir);
 		return retrieveHelper(query, limit);
 	}
-	
-	public static List<Entity> filter(String property, String keyword, String sortDirection, int limit) {
-		SortDirection dir = (sortDirection.equalsIgnoreCase("ascending"))? SortDirection.ASCENDING :SortDirection.DESCENDING;
-		Filter filter = new FilterPredicate(property, FilterOperator.EQUAL, keyword);
-		Query query = new Query("event").setFilter(filter).addSort("start_time", dir);
+
+	public static List<Entity> filter(String property, String keyword,
+			String sortDirection, int limit) {
+		SortDirection dir = (sortDirection.equalsIgnoreCase("ascending")) ? SortDirection.ASCENDING
+				: SortDirection.DESCENDING;
+		Filter filter = new FilterPredicate(property, FilterOperator.EQUAL,
+				keyword);
+		Query query = new Query("event").setFilter(filter).addSort(
+				"start_time", dir);
 		return retrieveHelper(query, limit);
 	}
-	
+
 	/**
 	 * Returns events starting today, or currently running today.
+	 * 
 	 * @return
 	 */
-	public static List<Entity> findEventsToday(){
+	public static List<Entity> findEventsToday() {
 		List<Entity> allEvents = retrieve("start_time", "descending", 50);
 		List<Entity> result = new ArrayList<Entity>();
-		for(Entity e: allEvents){
-			if (spansToday((String) e.getProperty("start_time"), (String) e.getProperty("end_time"))){
+		for (Entity e : allEvents) {
+			if (spansToday((String) e.getProperty("start_time"),
+					(String) e.getProperty("end_time"))) {
 				result.add(e);
 			}
 		}
 		return result;
 	}
-	
+
 	/**
-	 * Returns the events falling within the number of upcoming weeks from current date.
+	 * Returns the events falling within the number of upcoming weeks from
+	 * current date.
+	 * 
 	 * @param numWeeks
 	 * @return
 	 */
@@ -75,38 +89,40 @@ public class EventExtractor {
 		List<Entity> rawResult = retrieve("start_time", "ascending", 50);
 		List<Entity> result = new ArrayList<Entity>();
 		// filter results
-		for (Entity event: rawResult){
-			if (isInRange((String) event.getProperty("start_time"), numWeeks)){
-			result.add(event);
+		for (Entity event : rawResult) {
+			if (isInRange((String) event.getProperty("start_time"), numWeeks)) {
+				result.add(event);
 			}
 		}
 		return result;
 	}
-	
+
 	private static List<Entity> retrieveHelper(Query query, int limit) {
-		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-		List<Entity> events = datastore.prepare(query).asList(FetchOptions.Builder.withLimit(limit));
-		return events;	
+		DatastoreService datastore = DatastoreServiceFactory
+				.getDatastoreService();
+		List<Entity> events = datastore.prepare(query).asList(
+				FetchOptions.Builder.withLimit(limit));
+		return events;
 	}
-	
+
 	/**
 	 * returns true if a date falls within X weeks of current date
+	 * 
 	 * @return
 	 */
 	private static boolean isInRange(String otherDate, int numWeeks) {
-		DateTimeFormatter fmt = DateTimeFormat.forPattern("yyyy-MM-dd");
 		DateTime dateTime1 = new DateTime();
-		if (otherDate.length()== 10){
+		if (otherDate.length() == 10) {
 			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-	       try {
-			dateTime1 =  new DateTime(formatter.parse(otherDate));
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		}
-		else{
-			 dateTime1 = ISODateTimeFormat.dateTimeNoMillis().parseDateTime(otherDate);	
+			try {
+				dateTime1 = new DateTime(formatter.parse(otherDate));
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			dateTime1 = ISODateTimeFormat.dateTimeNoMillis().parseDateTime(
+					otherDate);
 		}
 		DateTime dateTime2 = new DateTime(new Date());
 		dateTime2 = dateTime2.plusWeeks(numWeeks);
@@ -114,46 +130,45 @@ public class EventExtractor {
 		long d2 = dateTime2.getMillis();
 		return (d1 < d2);
 	}
-	
-	private static boolean isToday(String otherDate){
+
+	private static boolean isToday(String otherDate) {
 		DateTime d1 = new DateTime();
-		if (otherDate.length()== 10){
+		if (otherDate.length() == 10) {
 			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-	       try {
-			d1 =  new DateTime(formatter.parse(otherDate));
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		}
-		else{
-			 d1 = ISODateTimeFormat.dateTimeNoMillis().parseDateTime(otherDate);	
+			try {
+				d1 = new DateTime(formatter.parse(otherDate));
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			d1 = ISODateTimeFormat.dateTimeNoMillis().parseDateTime(otherDate);
 		}
 		DateTime d2 = new DateTime(new Date());
-		return (d1.getDayOfYear() == d2.getDayOfYear() && d1.getYear()==d2.getYear());
+		return (d1.getDayOfYear() == d2.getDayOfYear() && d1.getYear() == d2
+				.getYear());
 	}
-	
+
 	/**
 	 * returns true if an event spans through today
 	 * @param otherDate
 	 * @return
 	 */
 	private static boolean spansToday(String iStart, String iEnd){
-		if (iStart.equals(new String(""))){
+		if (iStart.equals(new String(""))) {
 			return false;
 		}
 		DateTime start = new DateTime();
-		if (iStart.length()== 10){
+		if (iStart.length() == 10) {
 			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-	       try {
-			start =  new DateTime(formatter.parse(iStart));
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		}
-		else{
-		start = ISODateTimeFormat.dateTimeNoMillis().parseDateTime(iStart);
+			try {
+				start = new DateTime(formatter.parse(iStart));
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			start = ISODateTimeFormat.dateTimeNoMillis().parseDateTime(iStart);
 		}
 		DateTime d2 = new DateTime(new Date());
 		boolean condition2 = false;
